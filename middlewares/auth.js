8594e4cd446716cef10bbffc1f01ceacb2a5da19
AnishSinghChauhan <anishthe01@gmail.com>
2024-10-22 13:23:04 +0530

    Fixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,33 +1,83 @@
+// const { getUser } = require("../service/auth");
+
+// async function restrictToLoggedinUserOnly(req, res, next) {
+//   const userUid = req.cookies?.uid;
+
+//   if (!userUid) {
+//     return res.status(401).json({
+//         success: false,
+//         message: "User not logged in",
+//     });
+// }
+
+//   const user = getUser(userUid);
+
+//   if (!user) {
+//     return res.status(401).json({
+//         success: false,
+//         message: "Unauthorized",
+//     });
+// }
+
+//   req.user = user;
+//   next();
+// }
+
+// async function checkAuth(req, res, next) {
+//   const userUid = req.cookies?.uid;
+
+//   const user = getUser(userUid);
+
+//   req.user = user;
+//   next();
+// }
+
+// module.exports = {
+//   restrictToLoggedinUserOnly,
+//   checkAuth,
+// };
+
 const { getUser } = require("../service/auth");
 
 async function restrictToLoggedinUserOnly(req, res, next) {
-  const userUid = req.cookies?.uid;
-
-  if (!userUid) {
+  const authHeader = req.headers['authorization'];
+  
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({
-        success: false,
-        message: "User not logged in",
+      success: false,
+      message: "User not logged in",
     });
-}
+  }
 
-  const user = getUser(userUid);
+  const sessionId = authHeader.split(' ')[1]; // Extract session ID from Bearer token
+  
+  const user = getUser(sessionId);
 
   if (!user) {
     return res.status(401).json({
-        success: false,
-        message: "Unauthorized",
+      success: false,
+      message: "Unauthorized",
     });
-}
+  }
 
   req.user = user;
   next();
 }
 
 async function checkAuth(req, res, next) {
-  const userUid = req.cookies?.uid;
+  const authHeader = req.headers['authorization'];
+  
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized",
+    });
+  }
 
-  const user = getUser(userUid);
-
+  const sessionId = authHeader.split(' ')[1]; // Extract session ID from Bearer token
+  
+  const user = getUser(sessionId);
+  
   req.user = user;
   next();
 }
